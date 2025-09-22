b808b4ad25763cf4e2f00f5b3a871f195c6e6641
ElFlemmo <flemmingdn@msn.com>
2025-09-22 08:51:19 +0200

    2

--- serverConnect.sh
+++ serverConnect.sh
@@ -1,16 +1,14 @@
 ssh rumm-server
 strOnk053369strOnk0533
 
-cd /data/Flemming/DNAsense/quast
-git reset --hard HEAD
-git pull
-chmod +x /data/Flemming/git/metagenomics/quast.sh
+git clone https://github.com/ElFlemmo/metagenomics.git
+cd ~/metagenomics
+chmod +x QUAST.sh
 
 cd /data/Flemming/DNAsense/quast
-
 conda activate quast
 
-nohup /data/Flemming/git/metagenomics/quast.sh > pipeline.log 2>&1 &
+nohup bash "$HOME/metagenomics/QUAST.sh" > pipeline.log 2>&1 &
 
 tail -n 50 -f pipeline.log
 
